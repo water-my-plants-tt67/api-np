80fd4290fa7dcdef63fe1d70d9cf36f5213cb716
Nathaniel Patterson <odst0016@gmail.com>
2020-12-27 08:25:00 -0500

    nothing to see

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -4,4 +4,8 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "Plants up" });
 });
 
+router.get("/test", (req, res) => {
+  res.status(200).json({ message: "getting git configured on new os" });
+});
+
 module.exports = router;
